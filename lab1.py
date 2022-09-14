0a417d695dd5db31fc9d261a152003cc4a0ff103
Julia-Orlova <66700142+Julia-Orlova@users.noreply.github.com>
2022-09-14 16:58:16 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from math import exp, erf
 
 # вхідні дані
 a = 5 ** 13
@@ -36,20 +37,40 @@ average = sum(x) / n
 dispersion = sum([(x[i] - average) ** 2 for i in range(n)]) / (n - 1)
 print('Середнє арифметичне значення = {0}\nДисперсія = {1}'.format(round(average, 3), round(dispersion, 3)))
 
-# очікувана за теоретичним законом розподілу кількість влучень в і-ий інтервал
-np = [n * (ends_of_intervals[i+1] - ends_of_intervals[i]) / (max(x) - min(x)) for i in range(len(ends_of_intervals) - 1)]
-
 # перевірка відповідності досліджуваних випадкових чисел рівномірному закону розподілу
-criterion = sum([(y[i] - np[i]) ** 2 / np[i] for i in range(len(y))])
-print('Табличне значення критерію, взяте при рівні значимості α=0.05 та кількості степенів свободи 19, дорівнює 30.1.')
-print('Розраховане значення критерію: ', round(criterion, 6))
-if criterion <= 30.1:
-    print('{0} < 30.1, отже з довірчою ймовірністю 0,95 можна стверджувати, що знайдений закон розподілу\nвідповідає '
-          'спостережуваним значенням випадкової величини.'.format(round(criterion, 1)))
-else:
-    print('{0} > 30.1, отже з довірчою ймовірністю 0,95 можна стверджувати, що знайдений закон розподілу\nне '
-          'відповідає спостережуваним значенням випадкової величини.'.format(round(criterion, 1)))
+# очікувана за теоретичним законом розподілу кількість влучень в і-ий інтервал
+np_even = [n * (ends_of_intervals[i+1] - ends_of_intervals[i]) / (max(x) - min(x)) for i in range(len(ends_of_intervals) - 1)]
+criterion_even = sum([(y[i] - np_even[i]) ** 2 / np_even[i] for i in range(len(y))])
 
+# перевірка відповідності досліджуваних випадкових чисел експотенціальному закону розподілу
+lambd = 1 / average
+np_exponential = [n * (exp(- lambd * ends_of_intervals[i]) - exp(- lambd * ends_of_intervals[i+1]))
+                  for i in range(len(ends_of_intervals) - 1)]
+criterion_exponential = sum([(y[i] - np_exponential[i]) ** 2 / np_exponential[i] for i in range(len(y))])
+
+# перевірка відповідності досліджуваних випадкових чисел нормальному закону розподілу
+np_normal = [n * (erf((ends_of_intervals[i+1] - average) / (dispersion * 2) ** (1/2))
+                  - erf((ends_of_intervals[i] - average) / (dispersion * 2) ** (1/2))) / 2
+                  for i in range(len(ends_of_intervals) - 1)]
+criterion_normal = sum([(y[i] - np_normal[i]) ** 2 / np_normal[i] for i in range(len(y))])
+
+print('Табличне значення критерію, взяте при рівні значимості α=0.05 та кількості степенів свободи 19, дорівнює 30.1.')
+print('Розраховане значення критеріїв: рівномірний - {0}, експотенціальний - {1}, нормальний - {2}'
+      .format(round(criterion_even, 3), round(criterion_exponential, 3), round(criterion_normal, 3)))
+
+if criterion_even <= 30.1:
+    print('{0} < 30.1, отже з довірчою ймовірністю 0,95 можна стверджувати,\nщо випадкова величина розподілена '
+          'за рівномірним законом розподілу.'.format(round(criterion_even, 1)))
+elif criterion_exponential <= 30.1:
+    print('{0} > 30.1, отже з довірчою ймовірністю 0,95 можна стверджувати,\nщо випадкова величина розподілена '
+          'за експотенціальним законом розподілу.'.format(round(criterion_exponential, 1)))
+elif criterion_normal <= 30.1:
+    print('{0} > 30.1, отже з довірчою ймовірністю 0,95 можна стверджувати,\nщо випадкова величина розподілена '
+          'за нормальним законом розподілу.'.format(round(criterion_normal, 1)))
+else:
+    print('З довірчою ймовірністю 0,95 можна стверджувати, що випадкова величина\nне розподілена за рівномірним,'
+          ' нормальним та експотенціальним законами розподілу.')
+          
 # побудова гістограми
 fig, ax = plt.subplots()
 ax.bar(ends_of_intervals[0:20], y, width=h-0.005)
